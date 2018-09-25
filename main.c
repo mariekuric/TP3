1fe80636cac2f3ea03c9d92ad7875a2fc01495f2
iribs <iribs97@users.noreply.github.com>
2018-09-25 18:08:49 -0300

    Arreglo tokens y agregado de main.c

---
+++ main.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+char *nombres_token[]={"", "Identificador", "Constante" "programa" "variables" "codigo","fin", "leer", "escribir", "definir", 
+"+", "-", "*", "/", "-", "Asignacion","(", ")", ",", ".", "Error lexico","Constante invalida", "Identificador invalido"}
