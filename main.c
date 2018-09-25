48c59471fdf8be53ae96a6bdaf72397c26e8d043
iribs <iribs97@users.noreply.github.com>
2018-09-25 18:19:52 -0300

    main.c

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <scanner.l>
+
 
 char *nombres_token[]={"", "Identificador", "Constante" "programa" "variables" "codigo","fin", "leer", "escribir", "definir", 
 "+", "-", "*", "/", "-", "Asignacion","(", ")", ",", ".", "Error lexico","Constante invalida", "Identificador invalido"}
+
+int main(){
+	enum token t;
+	while ((t=yylex())){
+		if (t==1 | t==2 |t==21 | t==22 | t==23)
+			printf ("Token: %s\tLexema: %s\n", nombres_token[t], yytext);
+		else
+			printf ("Token: %s\n", nombres_token[t]);
+	}
+	printf ("Token: Fin de archivo");
+}
